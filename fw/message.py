8e49fe2732ecbe37047520981496c826a273959b
Colter Nakai McCarty <cnm62@nau.edu>
2016-03-22 08:29:51 -0700

    Remove destination property from Message class

--- fw/message.py
+++ fw/message.py
@@ -3,14 +3,17 @@ import copy
 class Message():
     def __init__(self, source=None, payload=None):
         self.source = []
-        #self.source.append(source)
-        self.destination.append(int(destination))
+        if source != None:
+            self.source.append(source)
+
         self.payload = payload
+
 
     def __str__(self):
         return("Source path\t" + str(self.source) + "\n" +
                "Destination\t" + str(self.destination) + "\n" +
                "\tPayload\t" + str(self.payload))
+
 
     def copy(self):
         return copy.deepcopy(self)
