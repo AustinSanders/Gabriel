6876d5aa5797bc7daf00284a54c02960d36ca9ab
Colter Nakai McCarty <cnm62@nau.edu>
2016-03-29 11:42:27 -0700

    CHANGED FW/__INIT__ SO THAT FILES CAN IMPORT FW
    I'm not sure how the code works or is supposed to work that is commented
    out in fw/__init__.py, but what is uncommented there now allows other
    files to simply 'import fw' and then reference the classes within the fw
    package as 'fw.ArchElement' for example.

--- fw/__init__.py
+++ fw/__init__.py
@@ -1,4 +1,9 @@
-from os.path import dirname, basename, isfile
-import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+# from os.path import dirname, basename, isfile
+# import glob
+# modules = glob.glob(dirname(__file__)+"/*.py")
+# __all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+
+from .arch_element import ArchElement
+from .component import Component
+from .connector import Connector
+from .message import Message
