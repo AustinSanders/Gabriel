94e1bb7f3ec81447f29fcb27d78454c87ace8bd4
Colter Nakai McCarty <cnm62@nau.edu>
2016-03-29 09:25:01 -0700

    MAKE COMPONENT CORRECTLY INHERIT FROM ARCHELEMENT
    Also migrated behavior from component that deals with the fields of
    ArchElement upward into the ArchElement class.

--- fw/arch_element.py
+++ fw/arch_element.py
@@ -1,7 +1,8 @@
 import threading
 from queue import Queue
 
-class Arch_Element(threading.Thread):
+
+class ArchElement(threading.Thread):
 
     def __init__(self, id, behavior=None):
         super().__init__(self)
@@ -13,77 +14,89 @@ class Arch_Element(threading.Thread):
         self._properties = {}
         self.requests = Queue()
         self.notifications = Queue()
-        if behavior != None:
+        if behavior is not None:
             self.run = behavior
-
 
     def __str__(self):
         return "Element: " + str(self._id)
-
 
     @property
     def id(self):
         """Getter for unique ID"""
         return self._id
 
-
     @property
     def properties(self):
         """Getter for properties"""
         return self._properties
-
 
     @property
     def top(self):
         """Getter for top port"""
         return self._top
 
-
     @property
     def bottom(self):
         """Getter for bottom port"""
         return self._bottom
 
+    @property
+    def send_to_all_handler(self, handler):
+        """Sets the send to all handler for this component"""
+        self._send_to_all_handler = handler
 
     def type(self):
         """Return the class name of the architectural element"""
         return self.__class__.__name__
 
-
     def add_top(self, listener):
         """Adds an event listener to the top port"""
         self._top.append(listener)
-
 
     def remove_top(self, listener):
         """Remove an event listener from the top port"""
         if (listener in self._top):
             self._top.remove(listener)
             return
-        print("Event listener ", listener,"not found / removed on connector ", self._id)
-
+        print("Event listener ", listener, "not found / removed on connector ", self._id)
 
     def add_bottom(self, listener):
         """Adds an event listener to the bottom port."""
         if (listener in self._bottom):
             self._bottom.remove(listener)
             return
-        print("Event listener ", listener," not found / removed on connector ", self._id)
+        print("Event listener ", listener, " not found / removed on connector ", self._id)
 
+    def property_names(self):
+        """Returns a list of the names of all properties in the element"""
+        prop_names = []
+        for key in self._properties:
+            prop_names.append(key)
+        return prop_names
+
+    def update_property(self, prop, value):
+        prop = str(prop)
+        self._properties[prop] = value
+
+    def remove_property(self, prop):
+        """Delete the specified property from the table if it exists"""
+        prop = str(prop)
+        if(prop in self._properties):
+            del self._properties[prop]
+        print('No property found with name ' + prop)
 
     def request(self, message):
         """Sends the specified message to all components listening to its top port"""
-        assert ( message.source is not None )
+        assert (message.source is not None)
         for i in self._top:
             c_message = message.copy()
             i.requests.put(c_message.append_sender(self))
 
-
     def notify(self, message):
         """Sends the specified message to all components listening to its bottom port"""
-        assert ( message.source is not None )
+        assert (message.source is not None)
         for i in self._bottom:
             c_message = message.copy()
             i.notifications.put(c_message.append_sender(self))
 
-#@TODO Implement interface style functions (i.e. raise NotImplementedErrors)
+# @TODO Implement interface style functions (i.e. raise NotImplementedErrors)
--- fw/component.py
+++ fw/component.py
@@ -1,136 +1,17 @@
+class Component(ArchElement):
 
-# component.py
-# Superclass for components and connectors
-
-import multiprocessing
-class Component(multiprocessing.Process):
-
-    def __init__(self, id):
-        super().__init__()
-        self._top = None #port for sending requests
-        self._bottom = None #port for sending notifications
-        self._arch_message_listeners = [] #port for sending architectural messages
-        self._send_to_all_handler = None
-        self._id = id #unique identifier for this component
-        self._properties = {} #table holding properties of the component
-
+    def __init__(self, id, behavior=None):
+        super().__init__(self, id, behavior)
 
     #@TODO replace "Component" with class name?
     def __str__(self):
         return "Component: " + str(self._id)
 
-
-    @property
-    def id(self):
-        """Getter for unique ID"""
-        return self._id
-
-
-    @property
-    def properties(self):
-        """Getter for properties"""
-        return self._properties
-
-    @property
-    def top(self):
-        """Getter for top interface"""
-        return self._top
-
-    @property
-    def bottom(self):
-        """Getter for bottom interface"""
-        return self._bottom
-
-    def type(self):
-        """Returns the class name as a string"""
-        return self.__class__.__name__
-
-
-    def add_top(self, listener):
-        """Welds the event listener to the top port."""
-        if self._top == None:
-            self._top = listener
-        else:
-            raise C2ArchException("A component may have only a single event " +
-                                  "listener per port")
-
-    def remove_top(self):
-        """Removes the event listener from the top port."""
-        self._top = None
-
-    def add_bottom(self, listener):
-        """Welds the event listener to the bottom port."""
-        if self._bottom == None:
-            self._bottom = listener
-        else:
-            raise C2ArchException("A component may have only a single event " +
-                                  "listener per port")
-
-    def remove_bottom(self):
-        """Removes the event listener from the bottom port."""
-        self._bottom = None
-
 ##@TODO## addRawMessageListener:
 
 ##@TODO## removeRawMessageListener:
 
-    def request(self, message):
-        """Sends the specified message to the components listening to its top
-        port"""
-        assert ( message.source is not None )
-        c_message = message.copy()
-        c_message.source.append(self.id)
-        self.top.request_sent(c_message)
-
-    def notify(self, message):
-        """Sends the specified message to the component connected to the
-        bottom port"""
-        assert ( message.source is not None )
-        c_message = message.copy()
-        c_message.source.append(self.id)
-        self.bottom.notification_sent(c_message)
-
-    @property
-    def send_to_all_handler(self, handler):
-        """Sets the send to all handler for this component"""
-        self._send_to_all_handler = handler
-
-##### sendToAll
-#    def send_to_all(self, message, interface):
-#        """Sends the specified message to all components welded to the specified interface"""
-#       assert (message is not None and interface is not None)
-#      idp = interface.interface_id_pair
-#     destinations = interface.get_all_connected_intefaces()
-#
 #TODO:
-##### getAllIntefaceIDs:
-##### hasInterface:
-
-    def property_names(self):
-        """Returns a list of the names of all properties in the component"""
-        prop_names = []
-        for key in self._properties:
-            prop_names.append(key)
-        return prop_names
-
-
-    def set_property(self, prop, value):
-        prop = str(prop)
-        self._properties[prop] = value
-
-
-    def remove_property(self, prop):
-        """Delete the specified property from the table if it exists"""
-        prop = str(prop)
-        if(prop in self._properties):
-            del self._properties[prop]
-        print('No property found with name ' + prop)
-
-##### addArchMessageListener
-##### removeArchMessageListener
-##### sendArchMessage
-
-#(implemented by child class)
 ##### getInterface
     def get_interface(id):
         raise NotImplementedError('Get_interface(id) must be overridden by child class')
