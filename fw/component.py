372bc6dfcb6f7b31fe61132efa747325ef45af53
Colter Nakai McCarty <cnm62@nau.edu>
2016-04-10 18:08:28 -0700

    ADD DEFAULT BEHAVIOR TO COMPONENT
    As the title says. Additionally, the component will default to it's
    behavior() method at initialization if the constructor is not passed a
    behavior callable. This means that the default behavior of a component
    can be overridden after it is initialized (or even changed while it is
    running!) simply by reassigning its behavior attribute to a new
    callable.

--- fw/component.py
+++ fw/component.py
@@ -2,5 +2,13 @@ import fw
 
 class Component(fw.ArchElement):
 
-    def __init__(self, id, behavior=None):
-        super().__init__(id, behavior)
+    def behavior(self):
+        for dispatcher in self.event_dispatchers:
+            dispatcher.dispatch_event()
+
+    def __init__(self, id, passed_behavior=None):
+        if passed_behavior is None:
+            super().__init__(self, id, self.behavior)
+        else:
+            super().__init__(self, id, passed_behavior)
+
