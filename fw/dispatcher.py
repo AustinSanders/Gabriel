1923941da90542e2040e8e650f3ec8257ad20b24
Colter Nakai McCarty <cnm62@nau.edu>
2016-05-04 21:41:58 -0700

    RENAME EVENT_DISPATCHER

--- fw/dispatcher.py
+++ fw/dispatcher.py
@@ -1,8 +1,15 @@
 from queue import Queue
 
-
+"""An EventDispatcher is a container for inbound Events and a collection of Event_Handlers that are
+will be allowed a chance to handle all received events serially. They have an unique identifier for
+locating them within containers. They are also constructed with three configuration parameters:
+blocking, timeout, and throwing. Respectively, these determine whether the event dispatcher will
+block when it is waiting to fetch an event from its internal Queue; what timeout (in TODO: units) to
+wait when trying to fetch an event, and whether or not the dispatcher will throw or silently swallow
+the Empty exception that is thrown when an attempt is made to dispatch an event and none are
+available."""
 class EventDispatcher(Queue):
-    def __init__(self, id, owner, blocking=False, timeout=None, throwing=False):
+    def __init__(self, id, blocking=False, timeout=None, throwing=False):
         Queue.__init__(self)
         self.id = id
         self.owner = owner
@@ -22,6 +29,9 @@ class EventDispatcher(Queue):
 
     def remove_handler(self, event_handler_id):
         self.event_handlers.remove(self.get_event_handler(event_handler_id))
+
+    def fire_event(self, event):
+        self.put(event)
 
     def dispatch_event(self):
         try:
