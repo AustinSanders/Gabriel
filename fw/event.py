53e96dfc73270d67ca25610bad5cee40364bc831
Colter Nakai McCarty <cnm62@nau.edu>
2016-05-04 21:38:55 -0700

    CHANGE event.py FROM A CLASS MODULE TO A FUNC MODULE

--- fw/event.py
+++ fw/event.py
@@ -1,62 +1,32 @@
 import copy
 
+"""Events are the base unit of communication between ArchElements. They are currently realized as
+plain ol', built-in python dictionaries. What makes them special is not *what* they are, but *how*
+they are used and how their use is constrained.
 
-class Event():
-    """Events are the base unit of communication between ArchElements. Events have two interesting
-    properties: they must only be mutated by their creator and must not be mutated after they are
-    sent. This is because, to increase the speed of Event passing, Events are passed by reference
-    rather than by value. As a result, any modification to an Event will be seen by *all*
-    ArchElements that receive and act on the same Event after it is modified.
+Gabriel objects make only one assumption about Events: Events received from other objects are
+immutable. The corollary is that events created by an object are only mutable until they are sent.
+These assumptions exist for two reasons.
+    1. As long as these assumptions hold invariably accross the system, Events can be passed by
+       reference rather than by value. This results in a decent speed increase in systems with large
+       quantities of events being passed around rapidly since events are not copied unless an object
+       needs to mutate their contents. This means that the number of copies that are created out of
+       necessity is the minimal number of copies which can be created.
 
-        Ex.
-            Suppose that there are three ArchElements connected like this:
+    2. As long as these assumptions hold invariably accross the system, Events will behave as they
+       would if they were passed by value. That is, once initially sent, they will never change, and
+       so there is no worry that an Event held by an object will be invalidated by another object
+       after it is received.
+       
+As such, the assumption of all utility methods in this module is that the event passed in as a
+parameter is not owned by the caller. Therefore, a copy is made before any alterations are
+performed."""
 
-                           /-->ElemB
-                    ElemA--|
-                           \-->ElemC
+# Appends the source parameter to a list in the event stored under the key 'source'
+def append_source(event, source):
+    event_copy = copy(event)
+    event_copy['source'] = source
+    return event_copy
 
-            Suppose A creates an Event 'E' and then sends it to both B and C.
-                    E = {payload : {source : 'A'
-                                    value  : "cool"}
-
-            Further suppose that B handles E before C and makes a changes it as follows:
-                    E = {payload : {source : 'B'
-                                    value  : "not so cool"}
-
-            Now, when C handles E, it will see E WITH THE CHANGES OF B! This could lead to total
-            component failure for C if it is depending on E's payload being simply "cool".
-
-    This may seem terrible, but in truth it is great because it requires that events must only be
-    copied when they are modified instead of every time they pass through a component.
-
-    To facilitate this, the payload field is prefixed with an underscore, and access to it is
-    provided by the payload() and payload_copy() methods. This will help remind developers to think
-    about how they are using the payload and whether a copy needs to be made or not. Finally, the
-    module level function make_from() facilitates the creation of a new Event from an old one."""
-
-    def __init__(self, payload=None):
-        self._payload = payload
-
-    def __str__(self):
-        return("\tSource:\t\t{0}\n\tPayload:\t{1}".format(self.source, self._payload))
-
-    def payload(self):
-        return self._payload
-
-    def payload_copy(self):
-        return copy.deepcopy(self._payload)
-
-    def append_source(self, source):
-        try:
-            self._payload["source"]
-        except KeyError:
-            e = make_from(self)
-            e._payload["source"] = [source]
-            return e
-        e = make_from(self)
-        e._payload["source"].append(source)
-        return e
-
-
-def make_from(other_event):
-    Event(other_event.payload_copy())
+def copy(event):
+    return copy.deepcopy(event)
